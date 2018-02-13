4ae2a3a2d957566f4cecee0246906dddadaad7c7
carlosteixeiraa <carlosteixeira.email@gmail.com>
2018-02-13 18:36:34 +0000

    Crawler added

---
+++ crawler/main.js
@@ -0,0 +1,43 @@
+var crawler = require('google'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    porta = 3020;
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+// http://localhost:3020/fsgey1r3i4?pedido=pao&resultados=10
+
+app.get('/:key', (req, res) => {
+    
+    var pedido = req.query.pedido;
+    var key = req.params.key;
+    var resultados = req.query.resultados;
+
+    crawler.resultsPerPage = resultados;
+
+    if(key === "fsgey1r3i4") {
+
+        crawler(pedido, (err, resposta) =>{
+
+            if (err) {
+                console.error(err)
+            } else {
+                res.send(resposta.links);
+            }
+
+        })
+
+    } else {
+        res.status(403);
+    }
+
+});
+
+app.listen(porta, () => {
+    console.log('Crawler a ouvir em localhost:' + porta)
+})
