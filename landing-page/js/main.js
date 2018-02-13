f2d01cac294b1c713c09708b8b2dcc009497265e
carlosteixeiraa <carlosteixeira.email@gmail.com>
2018-02-13 23:09:58 +0000

    Landing page done

---
+++ landing-page/js/main.js
@@ -0,0 +1,27 @@
+$(document ).ready(() => {
+    $('#texto h3').addClass('magictime puffIn');
+    $('#imagem img').addClass('magictime slideDownReturn');
+});
+  
+var app = document.getElementById('anim');
+
+var tw = new Typewriter(app, {
+    loop: true,
+    deleteSpeed: 25
+});
+
+tw.typeString('a search engine')
+    .pauseFor(750)
+    .deleteAll()
+    .typeString('voice controled')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('easy to use')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('open source')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('awessome')
+    .pauseFor(500)
+    .start();
