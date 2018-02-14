04ac26ade044a0fdb705e7ed5a8f2638aaece429
carlosteixeiraa <carlosteixeira.email@gmail.com>
2018-02-14 12:41:53 +0000

    fixed a bug

--- web-client/public/js/main.js
+++ web-client/public/js/main.js
@@ -32,27 +32,3 @@ celia.onresult = (event) => {
 celia.onend = () => {
   celia.start();
 }
-
-var queryjs = (function(a) {
-  if (a == "") return {};
-  var b = {};
-  for (var i = 0; i < a.length; ++i)
-  {
-      var p=a[i].split('=', 2);
-      if (p.length == 1)
-          b[p[0]] = "";
-      else
-          b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-  }
-  return b;
-})(window.location.search.substr(1).split('&'));
-
-var url = queryjs['s'];
-
-$("#irimg").click(() => {
-
-  var linkimg = "https://localhost:3000/imagens?s=" + url;
-  window.open(linkimg, '_self')
- 
-});
- 
